01a6e47d7532601e1273cceae09201ea3616ea7f
Marni Luka <marnixluka@gmail.com>
2023-04-24 19:50:52 -0400

    added variables and click event in JS

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Getcha head in the game")
+// Variables
+const math = document.querySelector(".math");
+const result = document.querySelector(".result");
+const calcBtn = document.querySelectorAll("button");
+
+// Button event
+calcBtn.forEach(number => {
+    number.addEventListener("click", calculate);
+})
+
+function calculate() {
+    console.log("check")
+}
