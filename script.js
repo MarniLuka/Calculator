7a6447c05ad3ec4d7b0747c43d7ddfcb2bca2404
Marni Luka <marnixluka@gmail.com>
2023-04-25 19:50:33 -0400

    added return in JS... cause I forgot... now only DE needs to be debugged

--- script.js
+++ script.js
@@ -14,18 +14,22 @@ function calculate() {
     if (displayText === "AC") {
         math.innerText = '';
         result.innerText = '';
+        return;
     }
 
     if (displayText === "DE") {
         math.innerText = math.value.toString().slice(0,-1);
+        return;
     }
 
     if (displayText === "=") {
-        result.innerText = eval(math.value);
+        result.innerText = eval(math.textContent);
+        return;
     }
 
     else {
         math.innerText += displayText;
+        return;
     }
 }
 
